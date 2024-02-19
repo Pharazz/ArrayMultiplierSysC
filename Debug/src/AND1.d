d977dbd1eef4f7132e269285591416871114b888
Pharazz <32317419+Pharazz@users.noreply.github.com>
2024-02-19 14:57:27 -0500

    Add files via upload

---
+++ Debug/src/AND1.d
@@ -0,0 +1,207 @@
+src/AND1.o: ../src/AND1.cpp C:\cygwin64\sysclibs\include/systemc.h \
+ C:\cygwin64\sysclibs\include/systemc \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_cmnhdr.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_externs.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_except.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_cmnhdr.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_module.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_kernel_ids.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_report.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_report_handler.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_process.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_constants.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_object.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_attribute.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_export.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_communication_ids.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_interface.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_typeindex.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_module_name.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_sensitive.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_time.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_nbdefs.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/fx/scfx_ieee.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/fx/sc_fxdefs.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_machine.h \
+ C:\cygwin64\sysclibs\include/sysc/packages/boost/detail/endian.hpp \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/fx/sc_fx_ids.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_nbutils.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bit_ids.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_int_ids.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_wait.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_simcontext.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_status.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_hash.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_pq.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_wait_cthread.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_logic.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_macros.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_mempool.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bit.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_process_handle.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_module.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_list.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_simcontext.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_process_handle.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_ver.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_writer_policy.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_buffer.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_signal.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_port.h \
+ C:\cygwin64\sysclibs\include/sysc/kernel/sc_event.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_prim_channel.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_ifs.h \
+ C:\cygwin64\sysclibs\include/sysc/tracing/sc_trace.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_clock.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_clock_ports.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_ports.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_event_finder.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_event_queue.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_export.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_fifo.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_fifo_ifs.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_fifo_ports.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_mutex.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_mutex_if.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_semaphore.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_semaphore_if.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_signal.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_ports.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_resolved.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_resolved_ports.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_rv.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_lv.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_lv_base.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bv_base.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bit_proxies.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_proxy.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_signed.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/misc/sc_value_base.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_temporary.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_length_param.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/fx/sc_context.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_nbexterns.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_unsigned.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_int_base.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_uint_base.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_rv_ports.h \
+ C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_rv.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bit.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_logic.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bv.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_lv.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_bigint.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_biguint.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_int.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_uint.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/misc/sc_concatref.h \
+ C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bv.h \
+ C:\cygwin64\sysclibs\include/sysc/tracing/sc_trace.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_utils_ids.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_pvector.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_string_view.h \
+ C:\cygwin64\sysclibs\include/sysc/utils/sc_vector.h ../src/AND1.h
+C:\cygwin64\sysclibs\include/systemc.h:
+C:\cygwin64\sysclibs\include/systemc:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_cmnhdr.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_externs.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_except.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_cmnhdr.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_module.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_kernel_ids.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_report.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_report_handler.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_process.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_constants.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_object.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_attribute.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_export.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_communication_ids.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_interface.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_typeindex.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_module_name.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_sensitive.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_time.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_nbdefs.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/fx/scfx_ieee.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/fx/sc_fxdefs.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_machine.h:
+C:\cygwin64\sysclibs\include/sysc/packages/boost/detail/endian.hpp:
+C:\cygwin64\sysclibs\include/sysc/datatypes/fx/sc_fx_ids.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_nbutils.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bit_ids.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_int_ids.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_wait.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_simcontext.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_status.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_hash.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_pq.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_wait_cthread.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_logic.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_macros.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_mempool.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bit.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_process_handle.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_module.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_list.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_simcontext.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_process_handle.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_ver.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_writer_policy.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_buffer.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_signal.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_port.h:
+C:\cygwin64\sysclibs\include/sysc/kernel/sc_event.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_prim_channel.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_ifs.h:
+C:\cygwin64\sysclibs\include/sysc/tracing/sc_trace.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_clock.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_clock_ports.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_ports.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_event_finder.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_event_queue.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_export.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_fifo.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_fifo_ifs.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_fifo_ports.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_mutex.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_mutex_if.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_semaphore.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_semaphore_if.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_signal.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_ports.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_resolved.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_resolved_ports.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_rv.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_lv.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_lv_base.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bv_base.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bit_proxies.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_proxy.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_signed.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/misc/sc_value_base.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_temporary.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_length_param.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/fx/sc_context.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_nbexterns.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_unsigned.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_int_base.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_uint_base.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_rv_ports.h:
+C:\cygwin64\sysclibs\include/sysc/communication/sc_signal_rv.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bit.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_logic.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bv.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_lv.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_bigint.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_biguint.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_int.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/int/sc_uint.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/misc/sc_concatref.h:
+C:\cygwin64\sysclibs\include/sysc/datatypes/bit/sc_bv.h:
+C:\cygwin64\sysclibs\include/sysc/tracing/sc_trace.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_utils_ids.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_pvector.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_string_view.h:
+C:\cygwin64\sysclibs\include/sysc/utils/sc_vector.h:
+../src/AND1.h:
---
+++ Debug/src/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/AND1.cpp \
+../src/ArrayMultiplier.cpp \
+../src/CPA.cpp \
+../src/CSA.cpp \
+../src/FullAdder.cpp \
+../src/sc_main.cpp 
+
+CPP_DEPS += \
+./src/AND1.d \
+./src/ArrayMultiplier.d \
+./src/CPA.d \
+./src/CSA.d \
+./src/FullAdder.d \
+./src/sc_main.d 
+
+OBJS += \
+./src/AND1.o \
+./src/ArrayMultiplier.o \
+./src/CPA.o \
+./src/CSA.o \
+./src/FullAdder.o \
+./src/sc_main.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cygwin C++ Compiler'
+	g++ -I"C:\cygwin64\sysclibs\include" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++14 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-src
+
+clean-src:
+	-$(RM) ./src/AND1.d ./src/AND1.o ./src/ArrayMultiplier.d ./src/ArrayMultiplier.o ./src/CPA.d ./src/CPA.o ./src/CSA.d ./src/CSA.o ./src/FullAdder.d ./src/FullAdder.o ./src/sc_main.d ./src/sc_main.o
+
+.PHONY: clean-src
+
